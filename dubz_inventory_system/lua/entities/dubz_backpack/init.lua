e929698ee663e69d5edcca3b1b19cc97b5807ead
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-24 16:20:41 -0500

    Add Dubz backpack inventory system

---
+++ dubz_inventory_system/lua/entities/dubz_backpack/init.lua
@@ -0,0 +1,51 @@
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("cl_init.lua")
+include("shared.lua")
+
+local config = DUBZ_BACKPACK and DUBZ_BACKPACK.Config or {}
+
+function ENT:Initialize()
+    self:SetModel(config.Model or self.Model)
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then
+        phys:Wake()
+        phys:SetMass(15)
+    end
+
+    self.StoredItems = self.StoredItems or {}
+end
+
+function ENT:Use(activator)
+    if not IsValid(activator) or not activator:IsPlayer() then return end
+
+    if activator:Crouching() then
+        self:PickupIntoWeapon(activator)
+        return
+    end
+
+    DUBZ_BACKPACK.SendInventory(activator, self, "entity")
+end
+
+function ENT:PickupIntoWeapon(ply)
+    if not IsValid(ply) or not ply:IsPlayer() then return end
+    if ply:HasWeapon("dubz_backpack") then
+        ply:ChatPrint("You are already carrying a backpack.")
+        return
+    end
+
+    local swep = ply:Give("dubz_backpack")
+    if not IsValid(swep) then return end
+
+    swep.StoredItems = table.Copy(self.StoredItems or {})
+    swep:SetNWBool("DubzBackpackCarried", true)
+
+    self:Remove()
+end
+
+function ENT:OnTakeDamage(dmg)
+    self:TakePhysicsDamage(dmg)
+end
