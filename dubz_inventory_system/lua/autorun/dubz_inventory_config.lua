54f928d2b008f67ec8adbb46149f4d0a0f00ee2f
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-24 22:42:39 -0500

    Pocket-style controls for inventory swep

---
+++ dubz_inventory_system/lua/autorun/dubz_inventory_config.lua
@@ -0,0 +1,16 @@
+DUBZ_INVENTORY = DUBZ_INVENTORY or {}
+
+DUBZ_INVENTORY.Config = {
+    Capacity = 10,
+    Category = "Dubz Utilities",
+    ColorBackground = Color(0, 0, 0, 190),
+    ColorPanel = Color(24, 28, 38),
+    ColorAccent = Color(25, 178, 208),
+    ColorText = Color(230, 234, 242),
+    PocketWhitelist = {
+        ["weapon_frag"] = true,
+        ["weapon_slam"] = true,
+        ["item_ammo_pistol"] = true
+    }
+}
+
