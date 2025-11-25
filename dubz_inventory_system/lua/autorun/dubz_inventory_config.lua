e263d11380bf01c86c7e84fb60cb3445bda717c0
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-25 01:45:22 -0500

    Refine inventory grid layout

---
+++ dubz_inventory_system/lua/autorun/dubz_inventory_config.lua
@@ -0,0 +1,15 @@
+DUBZ_INVENTORY = DUBZ_INVENTORY or {}
+
+DUBZ_INVENTORY.Config = {
+    Capacity = 10,
+    Category = "Dubz Utilities",
+    ColorBackground = Color(0, 0, 0, 190),
+    ColorPanel = Color(24, 28, 38),
+    ColorAccent = Color(25, 178, 208),
+    ColorText = Color(230, 234, 242),
+    -- Optional: if you want to restrict what non-weapon entities can be stored,
+    -- add them to PocketWhitelist. DarkRP's pocket blacklist is always honored
+    -- and will block anything defined there.
+    PocketWhitelist = {}
+}
+
