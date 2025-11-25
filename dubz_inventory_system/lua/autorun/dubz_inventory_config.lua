95aa2366f2965989baeba14b35b0e5cbcfeb5382
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-24 22:23:44 -0500

    Add themed inventory swep

---
+++ dubz_inventory_system/lua/autorun/dubz_inventory_config.lua
@@ -0,0 +1,11 @@
+DUBZ_INVENTORY = DUBZ_INVENTORY or {}
+
+DUBZ_INVENTORY.Config = {
+    Capacity = 10,
+    Category = "Dubz Utilities",
+    ColorBackground = Color(14, 16, 24),
+    ColorPanel = Color(24, 28, 38),
+    ColorAccent = Color(25, 178, 208),
+    ColorText = Color(230, 234, 242)
+}
+
